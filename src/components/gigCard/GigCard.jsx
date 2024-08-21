e5dfb39061d279ca3a81e518253ac91f8ccd4399
James Rosal <33856891+jamesrosal@users.noreply.github.com>
2024-08-21 16:16:57 -0500

    changes to gigs

--- src/components/gigCard/GigCard.jsx
+++ src/components/gigCard/GigCard.jsx
@@ -4,6 +4,7 @@ import './GigCard.scss';
 
 const GigCard = ({ item }) => {
   return (
+    // test commit
     <Link to='/gig/123'>
       <div className='gigCard'>
         <img src={item.img} alt='' />
